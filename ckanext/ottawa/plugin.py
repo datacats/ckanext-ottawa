3906122a0cd1b6515e86ee9961ba2014422d439a
Denis Zgonjanin <deniszgonjanin@gmail.com>
2014-12-17 15:58:33 -0500

    monkey patch our package display function to work with multilingual schema

--- ckanext/ottawa/plugin.py
+++ ckanext/ottawa/plugin.py
@@ -1,4 +1,6 @@
 import ckan.plugins as p
+import ckan.lib.helpers as h
+import json
 
 from ckan.plugins.interfaces import IDatasetForm
 from ckan.lib.plugins import DefaultDatasetForm
@@ -19,3 +21,14 @@ class OttawaThemePlugin(p.SingletonPlugin):
     def update_config(self, config):
         p.toolkit.add_template_directory(config, 'templates')
         p.toolkit.add_public_directory(config, 'public')
+
+        h.dataset_display_name = _dataset_display_name
+
+
+def _dataset_display_name(package_or_package_dict):
+    title = package_or_package_dict['title']
+    if isinstance(title, dict):
+        display = title[h.lang()]
+    else:
+        display = json.loads(title)[h.lang()]
+    return display
