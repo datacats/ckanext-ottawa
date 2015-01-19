c92245325206d281f86e56376754d483870a9924
Denis Zgonjanin <deniszgonjanin@gmail.com>
2015-01-19 11:43:51 -0500

    filter resource tags for uniques and non-uniques

--- ckanext/ottawa/plugin.py
+++ ckanext/ottawa/plugin.py
@@ -1,6 +1,7 @@
 import ckan.plugins as p
 import ckan.lib.helpers as h
 import json
+import logging
 
 from ckan.plugins.interfaces import IDatasetForm
 from ckan.lib.plugins import DefaultDatasetForm
@@ -10,10 +11,13 @@ from ckan.new_authz import is_sysadmin
 from ckan.logic.validators import name_validator
 from ckanext.scheming.plugins import SchemingDatasetsPlugin
 
+log = logging.getLogger(__name__)
+
 class OttawaThemePlugin(p.SingletonPlugin):
     p.implements(p.IConfigurable, inherit=True)
     p.implements(p.IRoutes, inherit=True)
     p.implements(p.IConfigurer, inherit=True)
+    p.implements(p.ITemplateHelpers, inherit=True)
 
     def configure(self, config):
         pass
@@ -25,6 +29,41 @@ class OttawaThemePlugin(p.SingletonPlugin):
 
         h.dataset_display_name = _dataset_display_name
 
+    def get_helpers(self):
+        return {
+            'resource_tags': _filter_resource_tags
+        }
+
+def _filter_resource_tags(package):
+    """
+    Returns resource links for a package such that any resource with a unique
+    format returns a link to the resource, and any link with a non-unique format
+    returns a link to the package page.
+    """
+    res_list = []
+    all_res = h.dict_list_reduce(package['resources'], 'format', unique=False)
+    uniques = [x for x in all_res if all_res.count(x) == 1]
+    non_uniques = set(all_res) - set(uniques)
+
+    for resource in package['resources']:
+        if resource['format'] in uniques:
+            res_list.append({
+                'format': resource['format'],
+                'url': h.url_for(controller='package',
+                                 action='resource_read',
+                                 id=package['name'],
+                                 resource_id=resource['id'])
+            })
+        elif resource['format'] in non_uniques:
+            res_list.append({
+                'format': resource['format'],
+                'url': h.url_for(controller='package',
+                                 action='read',
+                                 id=package['name'])
+            })
+            non_uniques.remove(resource['format'])
+
+    return res_list
 
 def _dataset_display_name(package_or_package_dict):
     title = package_or_package_dict['title']
